1cf908ddb43b0ca56e968552f09ec29fab123355
Amrender kuamr <amrenderk66@gmail.com>
2026-03-21 16:22:11 +0530

    add header.jsx in my github reposetries

--- frontend/src/comopnents/Header.jsx
+++ frontend/src/comopnents/Header.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div className='bg-gray-800 text-white p-4'>
-      <div>
-
+    <header className='bg-blue-100 backdrop-blur-md border border-white/30 rounded-2xl shadow-lg p-6 m-6'>
+      <div className='flex justify-between items-center gap-12'>
+        <h1 className='text-2xl font-bold text-gray-800 ml-6'>My App</h1>
+        <nav>
+          <ul className='flex space-x-7'>
+            <li><a href='#' className='text-gray-800 hover:text-gray-600 transition-colors duration-200 px-4 py-2 rounded-lg hover:bg-white/20'>Home</a></li>
+            <li><a href='#' className='text-gray-800 hover:text-gray-600 transition-colors duration-200 px-4 py-2 rounded-lg hover:bg-white/20'>About</a></li>
+            <li><a href='#' className='text-gray-800 hover:text-gray-600 transition-colors duration-200 px-4 py-2 rounded-lg hover:bg-white/20'>Contact</a></li>
+          </ul>
+        </nav>
       </div>
-    </div>
+    </header>
   )
 }
 
