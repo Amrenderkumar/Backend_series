7f38094ff35f5d0df169a54e1ed39cc2a6708e82
Amrender kuamr <amrenderk66@gmail.com>
2026-03-21 15:58:19 +0530

    databse in see the data is send or respone is correct

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,15 +1,22 @@
 import express from 'express';
+import  Data from '../models/post.model.js';
 
 
 const router = express.Router();
+router.post('/create_data', async (req, res) => {
+  try {
+    const data = await Data.create(req.body);
 
+    res.status(201).json({
+      message: "Data created successfully",
+      data
+    });
 
-router.post('/create_data', async (req,res) => {
-    req.body;
-    res.send('Data created successfully');
-    console.log(req.body);
-    
-})
+  } catch (error) {
+    console.error("ERROR:", error);
+    res.status(500).json({ message: "Error saving data" });
+  }
+});
 
 
 
