11ea2fac243c0aa1454bc6b2fb6cbd5a40c5ef26
Amrender kuamr <amrenderk66@gmail.com>
2026-03-22 17:26:42 +0530

    all code of header part

--- frontend/src/comopnents/Header.jsx
+++ frontend/src/comopnents/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className='bg-blue-100 backdrop-blur-md border border-white/30 rounded-2xl shadow-lg p-6 m-6'>
       <div className='flex justify-between items-center gap-12'>
-        <h1 className='text-2xl font-bold text-gray-800 ml-6'>My App</h1>
+        <h1 className='text-2xl font-bold text-gray-800 ml-6 cursor-pointer'>My App</h1>
         <nav>
           <ul className='flex space-x-7'>
             <li><a href='#' className='text-gray-800 hover:text-gray-600 transition-colors duration-200 px-4 py-2 rounded-lg hover:bg-white/20'>Home</a></li>
