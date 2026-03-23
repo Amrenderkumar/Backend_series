ac8cbf14f54faae3c6d32eac8603e117180d2179
Amrender kuamr <amrenderk66@gmail.com>
2026-03-23 13:22:01 +0530

    style chage

--- frontend/src/comopnents/Icons.jsx
+++ frontend/src/comopnents/Icons.jsx
@@ -3,7 +3,7 @@ import Img from '../assets/prog.webp'
 const Icons = () => {
   return (
     <div className='flex justify-end '>
-      <img className='h-120 w-120 rounded-full object-cover' src={Img} alt="Icon" />
+      <img className='h-120 w-120 rounded-full object-cover transition duration-300 hover:scale-x-[-1]' src={Img} alt="Icon" />
     </div>
   )
 }
--- frontend/src/comopnents/Middle.jsx
+++ frontend/src/comopnents/Middle.jsx
@@ -8,7 +8,7 @@ const Middle = () => {
                 <h2 className=' text-4xl font-bold mt-auto'>Demo Website</h2>
             </div>
             <div className='px-6 pl-35'>
-                <p className='text-left text-black font-medium  mt-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore praesentium quibusdam dolores alias. <br /> Saepe excepturi possimus est nulla repellat recusandae ut, porro mollitia aut ullam fuga <br /> nesciunt nisi qui at fugit delectus labore laboriosam quod quibusdam! Aliquid veritatis eligendi officiis!</p>
+                <p className='text-left text-black font-medium  mt-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore praesentium quibusdam <br /> dolores alias.  Saepe excepturi possimus est nulla repellat recusandae ut, porro mollitia aut ullam fuga <br /> nesciunt nisi qui at fugit delectus labore laboriosam quod quibusdam! Aliquid veritatis eligendi officiis!</p>
                 <button className='bg-blue-500 text-white py-3 px-5 mt-8 rounded-lg hover:bg-blue-600'>Get Started</button>
             </div>
             <div className='h-1/2 absolute top-40 right-10'>
