bcd6ca2614c3a2c8236ab5b8c2875ad21cb6b8ff
Amrender kuamr <amrenderk66@gmail.com>
2026-03-24 13:54:04 +0530

    just for demo purpuse ia have bulit in component demo.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Header} from './comopnents/Header'
 import Middle from './comopnents/middle'
+import Demo from './comopnents/Demo'
 const App = () => {
   return (
     <div>
      <Header />
      <Middle />
+     <Demo />
     </div>
   )
 }
---
+++ frontend/src/comopnents/Demo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Demo = () => {
+    return (
+        <div>
+            <div className='p-6 max-w-sm mx-auto bg-zinc-600 rounded-xl shadow-lg flex items-center space-x-4  mt-10'>
+                <div>
+                    <img className='h-12 w-12' src="https://www.randomcodegenerator.com/media/logo_social.png" alt="" />
+                </div>
+                <div>
+                    <div className='text-xl text-white font-medium '>
+                        Amrender Kumar
+                        <p className='text-slate-400'>My Channel Demo </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Demo
