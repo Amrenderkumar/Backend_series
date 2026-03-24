4b93f60c8664b5d6dd725595a8a49d490fe7eba5
Amrender kuamr <amrenderk66@gmail.com>
2026-03-24 20:08:43 +0530

    Agaim i try to breakpoint of the tailwind css properties

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {Header} from './comopnents/Header'
 import Middle from './comopnents/middle'
 import Demo from './comopnents/Demo'
+import Demo1 from './comopnents/Demo1'
 const App = () => {
   return (
     <div>
      <Header />
      <Middle />
      <Demo />
+     <Demo1 />
     </div>
   )
 }
---
+++ frontend/src/comopnents/Demo1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Demo1 = () => {
+    return (
+        <div className='mt-3'>
+            <div className=' max-w-sm mx-auto bg-black text-white rounded-xl overflow-hidden md:max-w-xl'>
+                <div className='md:flex'>
+                    <div>
+                        <img className='h-48 w-full object-cover md:h-full md:w-48' src="https://wallpapers.com/images/hd/random-people-walking-on-a-rainy-day-xibhqyyfg4prdneg.jpg" alt="" />
+                    </div>
+                    <div className='p-8'>
+                        <h1 className='md:text-yellow-800 font-medium text-xl hover:text-violet-800 cursor-pointer'>Title Content</h1>
+                        <p className='text-slate-400'>This is the description for the demo component</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Demo1
