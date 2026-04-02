47d88a7cb3ae1ed88eaacf4c2ee49914a6255068
Amrender kuamr <amrenderk66@gmail.com>
2026-04-03 00:50:50 +0530

    some changes of bg image grafix

--- frontend/src/comopnents/Components all/page2.jsx
+++ frontend/src/comopnents/Components all/page2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page2 = () => {
   return (
-    <div className='bg-slate-300 w-full h-[200px] flex items-center justify-center px-10'>
+    <div className='bg-slate-300 w-full h-52 flex items-center justify-center px-10 bg-[url("https://clipground.com/images/website-background-patterns-clipart-2.jpg")] bg-cover bg-center'>
       <div className='grid grid-cols-3 gap-6 w-full text-center'>
         <div>
           <p className='text-3xl font-bold text-black'>Production</p>
