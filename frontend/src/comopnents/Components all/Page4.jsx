08c493f76152fc76c70869f75c6578be19167c35
Amrender kuamr <amrenderk66@gmail.com>
2026-04-09 15:03:20 +0530

    some changes

--- frontend/src/comopnents/Components all/Page4.jsx
+++ frontend/src/comopnents/Components all/Page4.jsx
@@ -4,6 +4,8 @@ const Page4 = () => {
   return (
     <div>
       <h1>Page 4</h1>
+        <p>This is the content of Page 4. You can add more components or content here as needed.</p>
+        
     </div>
   )
 }
